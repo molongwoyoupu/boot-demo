ee65c4adc79534861b7105d5b3eccb12ab54d887
molong <liuj@daqsoft.com.cn>
2021-09-08 11:02:45 +0800

    注释

--- demo-api/src/main/java/com/demo/config/RestTemplateConfig.java
+++ demo-api/src/main/java/com/demo/config/RestTemplateConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * redis
+ * http服务配置
  *
  * @author molong
  * @date 2021/9/6
